aee4018cb39649f96d60d0d1c7a153f1ba3ccd8a
Kairen <sky4188@hotmail.com>
2016-08-03 21:57:10 +0800

    Day1Homework_v1.0

---
+++ Homework/Models/MoneyController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeWork.Models
+{
+    public class MoneyClass
+    {
+        public string MoneyType { get; set; }
+        public int MoneyAmount { get; set; }
+        public DateTime MoneyDate { get; set; }
+        public string MoneyNote { get; set; }
+
+        public int MoneyCount { get; set; }
+    }
+
+    
+}
